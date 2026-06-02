f3747df90723480518a011b54a74f7d28d172658
instincts <2606391015@qq.com>
2026-06-02 11:33:02 +0800

    edit

--- shap_single_image_seg.sh
+++ shap_single_image_seg.sh
@@ -1,6 +1,6 @@
 python shap_single_image_seg.py \
   --checkpoint /mnt/wangbd8/workspace/ThyroidAgent/dino_unet_ori/checkpoints/baseline/dino_unet_train_dataset_4_epoch_50.pth \
-  --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/ThyroidXL/test/images/ThyroidXL_test_00003189_31AD2E2C_1.png \
+  --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/ThyroidXL/test/images/ThyroidXL_train_00002730_C9690598_1.png \
   --output_dir ./pyradiomics_dice/shap_single_out \
   --img_size 224 \
   --dino_pretrained True \
