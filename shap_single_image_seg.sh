9d696893fa2b68e52fb47efdeb3d24f052da60b9
root <2606391015@qq.com>
2026-06-02 15:35:17 +0800

    edit

--- shap_single_image_seg.sh
+++ shap_single_image_seg.sh
@@ -1,7 +1,7 @@
 python shap_single_image_seg.py \
   --checkpoint /mnt/wangbd8/workspace/ThyroidAgent/dino_unet_ori/checkpoints/baseline/dino_unet_train_dataset_4_epoch_50.pth \
-  --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/ThyroidXL/test/images/TN3K_test_0010.png \
-  --mask_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/ThyroidXL/test/masks/TN3K_test_0010.png \
+  --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/TN3K/test/images/TN3K_test_0010.jpg \
+  --mask_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/train_val_test/TN3K/test/masks/TN3K_test_0010.jpg \
   --output_dir ./pyradiomics_dice/shap_single_out \
   --img_size 224 \
   --dino_pretrained True \
