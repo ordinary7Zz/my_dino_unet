7dbc7485c211cad46f6bbab891224353bda97db7
root <2606391015@qq.com>
2026-06-03 16:37:05 +0800

    edit

--- scripts/shap/shap_single_image_seg_LNM.sh
+++ scripts/shap/shap_single_image_seg_LNM.sh
@@ -1,5 +1,5 @@
 python shap_single_image_seg.py \
-  --checkpoint /mnt/wangbd8//mnt/wangbd8/workspace/ThyroidAgent/dino_unet_ori/checkpoints/train_Lymph_Node_Metastasis/dino_unet_Lymph_Node_Metastasis_epoch_40.pth \
+  --checkpoint /mnt/wangbd8/workspace/ThyroidAgent/dino_unet_ori/checkpoints/train_Lymph_Node_Metastasis/dino_unet_Lymph_Node_Metastasis_epoch_40.pth \
   --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Lymph_Node_Metastasis_fake/images/22_Benign_center1.png \
   --mask_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Lymph_Node_Metastasis_fake/masks/22_Benign_center1.png \
   --output_dir ./pyradiomics_dice/shap_single_out/LNM_CN01 \
