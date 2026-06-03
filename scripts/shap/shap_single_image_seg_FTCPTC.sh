f0881e804f8ddc8b76be728767f8654da2852112
instincts <2606391015@qq.com>
2026-06-03 16:55:29 +0800

    edit

--- scripts/shap/shap_single_image_seg_FTCPTC.sh
+++ scripts/shap/shap_single_image_seg_FTCPTC.sh
@@ -1,7 +1,7 @@
 python shap_single_image_seg.py \
   --checkpoint /mnt/wangbd8/workspace/ThyroidAgent/dino_unet_ori/checkpoints/baseline/dino_unet_train_dataset_4_epoch_50.pth \
-  --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Malignant_ultrasound_images_cropped/ \
-  --mask_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Malignant_ultrasound_images_cropped_predictions/ \
+  --image_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Malignant_ultrasound_images_cropped/FangDai/PTC/A_b17171017081122.png \
+  --mask_path /mnt/wangbd8/workspace/DataSets/ThyroidAgent/Classifaction_Data/Malignant_ultrasound_images_cropped_predictions/FangDai/PTC/A_b17171017081122.png \
   --output_dir ./pyradiomics_dice/shap_single_out/FTCPTC \
   --img_size 224 \
   --dino_pretrained True \
