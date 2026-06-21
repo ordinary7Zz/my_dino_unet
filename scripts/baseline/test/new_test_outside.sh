22b3d3819caeb448f52394d53fa255b492a51c7b
ordinary7Zz <2606391015@qq.com>
2026-06-21 12:52:41 +0800

    edit

--- scripts/baseline/test/new_test_outside.sh
+++ scripts/baseline/test/new_test_outside.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ---------------------- Configuration ----------------------
-Train_DATASET="dataset_4"
+Train_DATASET="dataset_6"
 # Set CUDA device
 CUDA_VISIBLE_DEVICES="1"
 # 根据 CUDA_VISIBLE_DEVICES 自动设置 device，也可以手动指定（如 "cuda:0", "cpu"）
